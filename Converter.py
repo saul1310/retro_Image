9570e2a8435d9e35a6de7ef2b2c94e68a0164938
unknown <stifshin@willamette.edu>
2024-07-03 19:59:26 -0700

    first array of pixels working :

--- Converter.py
+++ Converter.py
@@ -1,14 +1,25 @@
 from PIL import Image
 import os
+import numpy as np
 
 # Get the current directory of the script
 script_dir = os.path.dirname(__file__)
-
 # Define the relative path to the image
 image_path = os.path.join(script_dir, 'Example.jpg')
 
-# Open the image file
-image = Image.open(image_path)
+def image_to_array(image_path):
+    # Open an image file
+    with Image.open(image_path) as img:
+        # Convert the image to grayscale (optional, for simplicity)
+        img = img.convert('L')
+        # Convert the image to a numpy array
+        img_array = np.array(img)
+    return img_array
 
-# Display the image
-image.show()
+def main():
+    # Use the image_path defined earlier
+    img_array = image_to_array(image_path)
+    print(img_array)
+
+if __name__ == '__main__':
+    main()
