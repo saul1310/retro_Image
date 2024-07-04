c424063f60e8389fd9f4ec317a70f4209c699eea
unknown <stifshin@willamette.edu>
2024-07-04 13:23:40 -0700

    made function that creates new 2d matrix half the size of imported image file, to be variable in future

--- Converter.py
+++ Converter.py
@@ -9,17 +9,37 @@ image_path = os.path.join(script_dir, 'Example.jpg')
 
 def image_to_array(image_path):
     # Open an image file
+
+    # debugging statements to print image deatails
     with Image.open(image_path) as img:
-        # Convert the image to grayscale (optional, for simplicity)
-        img = img.convert('L')
+       
+        print(f"Image mode: {img.mode}")
+        print(f"Image size: {img.size}")
+        
         # Convert the image to a numpy array
         img_array = np.array(img)
     return img_array
 
+
+
+def reconstruct_new_image():
+    img_array = image_to_array(image_path)
+    new_array_size=len(img_array) // 2
+    new_array =[]
+
+    for i in range(new_array_size):
+        new_array.append([0,0,0])
+    print(new_array)
+
+
+ 
+
 def main():
+    
     # Use the image_path defined earlier
     img_array = image_to_array(image_path)
-    print(img_array)
+
+    reconstruct_new_image()
 
 if __name__ == '__main__':
     main()
