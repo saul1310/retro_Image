f80bf76b18024b255d29d4efd95825e1f010138a
unknown <stifshin@willamette.edu>
2024-07-05 14:49:36 -0700

    added documentation for traversal of 3d matrix

--- Converter.py
+++ Converter.py
@@ -7,6 +7,8 @@ script_dir = os.path.dirname(__file__)
 # Define the relative path to the image
 image_path = os.path.join(script_dir, 'Example.jpg')
 new_array_len = 0
+old_array_len = 0 
+
 
 
 
@@ -20,8 +22,10 @@ def image_to_array(image_path):
       
         img_array = np.array(img)
         global new_array_len
+        global old_array_len
+        old_array_len = len(img_array)
         new_array_len = len(img_array) //2
-        print(img_array[0][0][1])
+        print(img_array)
     
 
 
@@ -37,9 +41,26 @@ def create_new_array():
      return new_array
   
 def color_averager(img_array,new_array):
+    
+
     """ examines origional array, find average color for group of two pixels, and then assigns it to the new array"""   
 
     #  TIP -->to change a specific r g or b  value use a third index ie to access the blue value for a pixel use array[0][0][2] = 220
+        # img_array[0] accesses the first row of pixels in the image.
+        # img_array[0][0] accesses the first pixel in the first row.
+        # img_array[0][0][0] accesses the red channel value of the first pixel in the first row.
+        # img_array[0][0][1] accesses the green channel value of the first pixel in the first row.
+        # img_array[0][0][2] accesses the blue channel value of the first pixel in the first row.
+
+
+
+
+    # for i in range(old_array_len):
+    #     for j in range(3):
+            
+
+
+
 
   
         
